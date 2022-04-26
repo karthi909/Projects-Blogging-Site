c4011ba84f245cf8c0d8ef8de2681728c28bec19
karthi909 <karthikeyareddy440@gmail.com>
2022-04-26 22:11:02 +0530

    err

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,11 +1,42 @@
 const authorModel = require("../models/authorModel")
 const blogsModel = require("../models/blogsModel")
 
+
 const createAuthor= async function (req, res) {
     let data= req.body
     let savedData= await authorModel.create(data)
     res.send({msg: savedData})
 }
+
+
+const createBlogs = async function (req, res) {
+    try{
+        let blog = req.body
+        let authorid = req.body.authorId
+        console.log(authorid)
+
+        let author = await authorModel.findOne({_id: authorid}, {_id:1});
+
+        console.log(author)
+        if (authorid == null ) {
+            res.send({msg:' author Id missing'})
+        } 
+        
+         if( authorid !== author._id){
+            res.send({msg: 'invalid authorId'})
+        }
+
+        let createBlog = await blogsModel.create(blog)
+
+        res.status(201).send({msg: createBlog})
+    } 
+    catch (err) {
+        console.log(err)
+        res.status(500).send({msg: err.message})
+    }
+}
+
+module.exports.createBlogs = createBlogs
 
 module.exports.createAuthor= createAuthor
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const controllers = require("../controllers/controllers")
+const controllers = require("../controllers/controllers");
+const blogsModel = require('../models/blogsModel');
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -12,6 +13,8 @@ router.get("/test-me", function (req, res) {
 // router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createAuthor", controllers.createAuthor )
+
+router.post("/createBlog", controllers.createBlogs)
 
 
 module.exports = router;
